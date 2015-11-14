c6043d30dda2a5b4b4e48b5557498f695feadb37
Nic La <sakeofmaking@gmail.com>
2015-11-14 11:32:42 -0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,65 @@
+/******************************************************************************
+ * File Name:	Main.c
+ * Program:		Project for Fundamentals of Embedded Systems class
+ * Author:		Robert Weber
+ * Purpose:		Main file. Contains the Main function and other executive
+ *				functions. It's the "master" of the program.
+ *
+ *  Date	  By:		Changes:
+ * ---------  --------	-------------------------------------------------------
+ * 24 Apr 02  R Weber	Original file.
+ * 24 Feb 04  T Lill	modified for Winter 04 session
+ * 22 Apr 05  T Lill	removed deprecated functions
+ * 03 Nov 06  T Lill	Simplified heartbeat function
+ * 15 Dec 06  T Lill	Updated heartbeat function
+ * 01 Nov 10  T Lill	Converted program to ATmega2560
+ ******************************************************************************/
+#include "lib.h"
+#include "interrpt.h"
+#include "heartbeat.h"
+#include "serial.h"
+
+/************************* Function Prototypes ******************************/
+int main(void);
+
+
+/*********************** Function Implementations ******************************/
+
+void heartbeat(void)
+{
+	// see warning about SET_BIT in lib.h
+	asm volatile (" sbi	0x03, 0 ");
+}
+
+/*****************************************************************************
+ * Main function.
+ * 
+ * Initializes hardware and software. Then enters the endless foreground loop.
+ * 
+ *****************************************************************************/
+int main(void) 
+{
+    // Disable interrupts
+    cli();
+
+	// Set port B as an output and turn off all LEDs
+	DDRB = 0xFF;
+	PORTB = 0xFF;
+
+    /* Initialize the Timer 0 */
+    ISR_InitTimer0();
+
+	// Initialize serial I/O
+	SCIInitialize();
+
+    /* Enable interrupts. Do as last initialization, so interrupts are
+     * not initiated until all of initialization is complete. */
+   sei();
+
+   for ( ; ; )		/* Foreground loops forever */
+   {   // Do slow tasks here
+   
+   }   /* end of endless loop */
+
+   return 0;
+}
